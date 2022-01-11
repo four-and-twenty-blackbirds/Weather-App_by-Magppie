bbf5ff5e8d5e54f0a3af82f9c615fd13182796de
Magpie <megydoodles15@gmail.com>
2022-01-10 21:47:58 -0700

    Added conversion toggle btwn C & F

--- src/app.js
+++ src/app.js
@@ -41,25 +41,41 @@ function showCityWeather(response) {
 	let mainTemp = document.querySelector("#main-temp");
 	let hiTemp = document.querySelector("#hi-temp");
 	let loTemp = document.querySelector("#lo-temp");
+	let feelsLike = document.querySelector("#feels-like");
 	let icon = response.data.weather[0].icon;
 	let iconDescript = response.data.weather[0].description;
 	let currentCondIcon = document.querySelector("#current-conditions-icon");
 	let conditions = document.querySelector("#conditions");
-	let feelsLike = document.querySelector("#feels-like");
+
 	let humidity = document.querySelector("#humidity");
 	let windSpeed = document.querySelector("#wind-speed");
 
+	let mainUnit = document.querySelector("#main-unit");
+	mainUnit.innerHTML = "°F";
+	let hiUnit = document.querySelector("#hi-unit");
+	hiUnit.innerHTML = "°F";
+	let loUnit = document.querySelector("#lo-unit");
+	loUnit.innerHTML = "°F";
+	let feelsUnit = document.querySelector("#feels-unit");
+	feelsUnit.innerHTML = "°F";
+
+	mainFarTemp = response.data.main.temp;
+	hiFarTemp = response.data.main.temp_max;
+	loFarTemp = response.data.main.temp_min;
+	feelsFarTemp = response.data.main.feels_like;
+
 	cityName.innerHTML = response.data.name;
-	mainTemp.innerHTML = Math.round(response.data.main.temp);
-	hiTemp.innerHTML = Math.round(response.data.main.temp_max);
-	loTemp.innerHTML = Math.round(response.data.main.temp_min);
+	mainTemp.innerHTML = Math.round(mainFarTemp);
+	hiTemp.innerHTML = Math.round(hiFarTemp);
+	loTemp.innerHTML = Math.round(loFarTemp);
+	feelsLike.innerHTML = Math.round(feelsFarTemp);
+
 	currentCondIcon.setAttribute(
 		"src",
 		`http://openweathermap.org/img/wn/${icon}@2x.png`
 	);
 	currentCondIcon.setAttribute("alt", `The icon shows ${iconDescript}`);
 	conditions.innerHTML = response.data.weather[0].main;
-	feelsLike.innerHTML = Math.round(response.data.main.feels_like);
 	humidity.innerHTML = response.data.main.humidity;
 	windSpeed.innerHTML = Math.round(response.data.wind.speed);
 
@@ -83,7 +99,70 @@ function changeCity(event) {
 	search(cityInput.value);
 }
 
-search("Tucson");
+function convertC(event) {
+	event.preventDefault();
+	let mainTempC = Math.round(((mainFarTemp - 32) * 5) / 9);
+	let hiTempC = Math.round(((hiFarTemp - 32) * 5) / 9);
+	let loTempC = Math.round(((loFarTemp - 32) * 5) / 9);
+	let feelsTempC = Math.round(((feelsFarTemp - 32) * 5) / 9);
+
+	let changeMainTemp = document.querySelector("#main-temp");
+	changeMainTemp.innerHTML = Math.round(mainTempC);
+	let changeHiTemp = document.querySelector("#hi-temp");
+	changeHiTemp.innerHTML = Math.round(hiTempC);
+	let changeLoTemp = document.querySelector("#lo-temp");
+	changeLoTemp.innerHTML = Math.round(loTempC);
+	let changeFeelsLike = document.querySelector("#feels-like");
+	changeFeelsLike.innerHTML = Math.round(feelsTempC);
+
+	let mainUnit = document.querySelector("#main-unit");
+	mainUnit.innerHTML = "°C";
+	let hiUnit = document.querySelector("#hi-unit");
+	hiUnit.innerHTML = "°C";
+	let loUnit = document.querySelector("#lo-unit");
+	loUnit.innerHTML = "°C";
+	let feelsUnit = document.querySelector("#feels-unit");
+	feelsUnit.innerHTML = "°C";
+}
+
+function revertF(event) {
+	event.preventDefault();
+	let mainTempF = mainFarTemp;
+	let hiTempF = hiFarTemp;
+	let loTempF = loFarTemp;
+	let feelsTempF = feelsFarTemp;
+
+	let changeMainTemp = document.querySelector("#main-temp");
+	changeMainTemp.innerHTML = Math.round(mainTempF);
+	let changeHiTemp = document.querySelector("#hi-temp");
+	changeHiTemp.innerHTML = Math.round(hiTempF);
+	let changeLoTemp = document.querySelector("#lo-temp");
+	changeLoTemp.innerHTML = Math.round(loTempF);
+	let changeFeelsLike = document.querySelector("#feels-like");
+	changeFeelsLike.innerHTML = Math.round(feelsTempF);
+
+	let mainUnit = document.querySelector("#main-unit");
+	mainUnit.innerHTML = "°F";
+	let hiUnit = document.querySelector("#hi-unit");
+	hiUnit.innerHTML = "°F";
+	let loUnit = document.querySelector("#lo-unit");
+	loUnit.innerHTML = "°F";
+	let feelsUnit = document.querySelector("#feels-unit");
+	feelsUnit.innerHTML = "°F";
+}
+
+let mainFarTemp = null;
+let hiFarTemp = null;
+let loFarTemp = null;
+let feelsFarTemp = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", changeCity);
+
+let unitC = document.querySelector("#c-unit");
+unitC.addEventListener("click", convertC);
+
+let unitF = document.querySelector("#f-unit");
+unitF.addEventListener("click", revertF);
+
+search("Tucson");
