637bf8d05c4bfeec433d7d5063f2f883940d3ed5
Magpie <megydoodles15@gmail.com>
2022-01-10 18:26:46 -0700

    Grabbing data from API to replace in doc
    - Main temperature, hi & lo, conditions, & other current weather data
    - Using weather data for Tucson at the moment
    - Next: change date & time in document

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
-function showCurrentTemp(response) {
+function showCityWeather(response) {
 	console.log(response.data);
+
+	let mainTemp = document.querySelector("#main-temp");
+	mainTemp.innerHTML = Math.round(response.data.main.temp);
+
+	let hiTemp = document.querySelector("#hi-temp");
+	hiTemp.innerHTML = Math.round(response.data.main.temp_max);
+
+	let loTemp = document.querySelector("#lo-temp");
+	loTemp.innerHTML = Math.round(response.data.main.temp_min);
+
+	let conditions = document.querySelector("#conditions");
+	conditions.innerHTML = response.data.weather[0].main;
+
+	let feelsLike = document.querySelector("#feels-like");
+	feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+
+	let humidity = document.querySelector("#humidity");
+	humidity.innerHTML = response.data.main.humidity;
+
+	let windSpeed = document.querySelector("#wind-speed");
+	windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let city = "Tucson";
@@ -7,6 +28,4 @@ let unit = "imperial";
 let apiKey = "aee115d67b5ede7133bf4a0747025512";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
-console.log(apiUrl);
-
-axios.get(`${apiUrl}`).then(showCurrentTemp);
+axios.get(apiUrl).then(showCityWeather);
